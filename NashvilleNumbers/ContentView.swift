7838a4fe81499f24e5cb99975e74337e50f723bd
webbdm <gwebb762@gmail.com>
2021-10-02 20:03:10 -0500

    Initial Commit

---
+++ NashvilleNumbers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NashvilleNumbers
+//
+//  Created by Geoff Webb on 10/2/21.
+//  Copyright © 2021 Geoff Webb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NashvilleNumbersTests/NashvilleNumbersTests.swift
@@ -0,0 +1,34 @@
+//
+//  NashvilleNumbersTests.swift
+//  NashvilleNumbersTests
+//
+//  Created by Geoff Webb on 10/2/21.
+//  Copyright © 2021 Geoff Webb. All rights reserved.
+//
+
+import XCTest
+@testable import NashvilleNumbers
+
+class NashvilleNumbersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
