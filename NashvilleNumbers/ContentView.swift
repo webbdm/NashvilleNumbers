d1c3057351565a75102f7fd9566e6d162224e88d
webbdm <gwebb762@gmail.com>
2021-11-13 20:50:40 -0600

    NNI-001 Read json file and display to view

--- NashvilleNumbers/ContentView.swift
+++ NashvilleNumbers/ContentView.swift
@@ -1,17 +1,52 @@
-//
-//  ContentView.swift
-//  NashvilleNumbers
-//
-//  Created by Geoff Webb on 10/2/21.
-//  Copyright © 2021 Geoff Webb. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var keys: [Key] = []
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            VStack{
+                ZStack {
+                  Color.red
+                    .edgesIgnoringSafeArea(.all)
+                  
+                    HStack(alignment: .lastTextBaseline, spacing: 100){
+                        Text("Key").font(.title).foregroundColor(.blue)
+                        Text("Select")
+                        .font(.title)
+                        .foregroundColor(.blue)
+                        .padding()
+                        .overlay(
+                           RoundedRectangle(cornerRadius: 15)
+                               .stroke(Color.blue, lineWidth: 3)
+                        )
+                    }
+                    
+                }
+                .onAppear(perform: readFile)
+                
+                ForEach(keys) { key in
+                    NavigationLink(destination: KeyView(key: key)) {
+                        Text(key.keyName)
+                               }.navigationBarTitle(Text("KeyView"))
+                }
+            }
+        }
     }
+    
+    private func readFile(){
+            let url = Bundle.main.url(forResource: "keys", withExtension: "json")!
+            let data = try! Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let jsonData = try! decoder.decode(JSONData.self, from:data)
+            self.keys = jsonData.keys
+    }
+    
+}
+
+struct JSONData : Decodable {
+    let keys: [Key]
 }
 
 struct ContentView_Previews: PreviewProvider {
