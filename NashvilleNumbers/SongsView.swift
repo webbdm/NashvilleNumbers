d30ab93df834b827975be4c8a20b9b00b06e19a1
webbdm <gwebb762@gmail.com>
2021-12-25 00:23:56 -0600

    NNI-002 Songs pagestyling

--- NashvilleNumbers/SongsView.swift
+++ NashvilleNumbers/SongsView.swift
@@ -1,11 +1,3 @@
-//
-//  SongsView.swift
-//  NashvilleNumbers
-//
-//  Created by Geoff Webb on 12/19/21.
-//  Copyright © 2021 Geoff Webb. All rights reserved.
-//
-
 import SwiftUI
 
 struct SongsView: View {
@@ -19,46 +11,92 @@ struct SongsView: View {
     
     
     var body: some View {
-       VStack{
+        VStack{
         ZStack {
             Color("bluebg").edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 150){
+            VStack(spacing: 5){
                 VStack(){
                     Text("Songs")
-                    .padding(20)
+                    .padding(0)
                     .font(.system(size:56.0))
                     .foregroundColor(Color("lightb"))
-                    ForEach(songs) { song in
-                        HStack{
-                            Text(song.name).foregroundColor(Color(.white))
-                            Text(song.key).foregroundColor(Color("lightb"))
+                    ScrollView{
+                        ForEach(songs) { song in
+                            HStack{
+                                Text(song.name).foregroundColor(Color(.white))
+                                Text(song.key).foregroundColor(Color("lightb"))
+                            }
+                        }
+                    }.padding()
+                }
+            VStack(){
+                VStack(alignment: .leading){
+                    VStack(alignment: .leading){
+                    Text("Song Name").foregroundColor(Color("lightb"))
+                    TextField("Song Name",text: $name)
+                        .padding(.all, 10)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    Divider()
+                        .frame(height: 1)
+                        .padding(.horizontal, 30)
+                        .background(Color.gray)
+                   Text("Key").foregroundColor(Color("lightb")).padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                   TextField("Key",text: $key)
+                       .padding(.all, 10)
+                       .cornerRadius(10)
+                       .foregroundColor(.white)
+                    Divider()
+                        .frame(height: 1)
+                        .padding(.horizontal, 30)
+                        .background(Color.gray)
+                    }.padding(EdgeInsets(top: 30, leading: 0, bottom: 30, trailing: 0))
+                    
+                HStack{
+                    Text("Add Song").foregroundColor(Color("panel"))
+                     .font(.system(size:26.0))
+                    Image(systemName: "plus.circle")
+                      .padding().foregroundColor(Color("panel"))
+                }.frame(maxWidth: .infinity)
+                 .padding(15)
+                 .background(Color("lightb"))
+                 .cornerRadius(10)
+                 .onTapGesture(count: 2) {self.add()}
+            }.padding()
+                
+                Spacer()
+                HStack(spacing: 95){
+                    NavigationLink(destination: SongsView(songs: $songs)) {
+                        VStack{
+                           Image(systemName: "music.note")
+                          Text("Home").foregroundColor(.white)
+                       }
+                    }
+                    
+                    NavigationLink(destination: SongsView(songs: $songs)) {
+                        VStack{
+                            Image(systemName: "music.note.list")
+                            Text("Songs").foregroundColor(.white)
+                        }
+                    }
+                    
+                    NavigationLink(destination: SetlistView()) {
+                        VStack{
+                          Image(systemName: "music.note.list")
+                          Text("Setlists").foregroundColor(.white)
                         }
                     }
                 }
-            HStack{
-                VStack{
-                    TextField(
-                               "Song Name",
-                                text: $name
-                               )
-                               .padding(.all, 20)
-                               .background(Color("panel"))
-                               .cornerRadius(10)
-                               .foregroundColor(.white)
-                               
-                               TextField(
-                                   "Key",
-                                   text: $key
-                               )
-                               .padding(.all, 20)
-                               .background(Color("panel"))
-                               .cornerRadius(10)
-                               .foregroundColor(.white)
-                            
-                }
-                 Button("Add Song", action: add)
-              }.padding().background(Color("lightb"))
+                
+              }.frame(maxHeight:350)
+                .padding()
+                .background(
+                  RoundedCornersShape(corners: [.topLeft, .topRight], radius: 35)
+                      .fill(Color("panel"))
+                      .edgesIgnoringSafeArea(.bottom)
+                      
+              )
             }
          }
        }
